2fbc555dfd8996a5c536278f29bba4bbf17ffa00
gabe-dubose <gabe.dubose.sci@gmail.com>
2023-10-01 13:27:13 -0400

    Added initial rarefaction functionality

---
+++ setup.py
@@ -0,0 +1,23 @@
+from importlib.metadata import entry_points
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gnat",
+    version="2023.10",
+    author="Gabe DuBose",
+    author_email="gabe.dubose.sci@gmail.com",
+    description="Gabe's aNAlysis Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabe-dubose/gnat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['pandas']
+)
